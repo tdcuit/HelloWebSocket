ce13d1e59018005c72af5a35fa86fb4bda542703
tdcuit <806066058@qq.com>
2019-09-24 23:37:09 +0800

    init

---
+++ src/main/java/com/tdcuit/HelloWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.tdcuit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWebSocketApplication {
+	 public static void main(String[] args) {
+	        SpringApplication.run(HelloWebSocketApplication.class, args);
+	    }
+}
+
+
---
+++ src/main/java/com/tdcuit/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.tdcuit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class IndexController {
+    @GetMapping({ "/", "" })
+    public String index()  {
+        return "index";
+    }
+}
+
